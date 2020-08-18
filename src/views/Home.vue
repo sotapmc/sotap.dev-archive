15f2e7816391aafd90d7904045889025fcf14682
Subilan <suyuanliang1995@gmail.com>
2020-08-18 12:49:42 +0800

    Fix page overflow

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,24 @@
 <template>
 	<div class="home">
 		<h1>Build Our World Together.</h1>
-		<p>共同建立全新的 SoTap 社区</p>
+		<p>共同创造我们的世界</p>
 	</div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
+
+<style lang="less" scoped>
+h1 {
+	font-size: 56px;
+	margin-top: 16px;
+	margin-bottom: 16px;
+
+	& + p {
+    font-size: 18px;
+    letter-spacing: 4px;
+	}
+}
+</style>
