bd8f7fbf21903657aa897044078914dcbc5982d1
Subilan <suyuanliang1995@gmail.com>
2020-08-18 14:39:15 +0800

    Remove pwa feature from project

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,6 @@
 const path = require("path");
 
 module.exports = {
-    pwa: {
-        name: "SoTap Dev",
-        themeColor: "#000",
-        msTileColor: "#000",
-        appleMobileWebAppCapable: 'yes',
-        appleMobileWebAppStatusBarStyle: 'black',
-        workboxPluginMode: "GenerateSW"
-    },
-
     pluginOptions: {
       'style-resources-loader': {
         preProcessor: 'less',
