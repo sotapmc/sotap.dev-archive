a90f7002d9de2a494dce3e9c8c9c9543e0d27352
Subilan <suyuanliang1995@gmail.com>
2020-08-18 13:49:26 +0800

    Add animations

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,92 @@
 <template>
 	<div class="home">
-		<h1>Build Our World Together.</h1>
-		<p>共同创造我们的世界</p>
+		<span class="hero-wrapper">
+			<h1 class="hero">Build Our World Together.</h1>
+		</span>
+		<p>
+			一起造<ruby>轮<rt>lún</rt>子<rt>zi</rt></ruby
+			>让世界变得更好吧
+		</p>
 	</div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-export default Vue.extend({});
+export default Vue.extend({
+	mounted() {
+		this.$anime.timeline({ loop: false }).add({
+			targets: ".hero",
+			translateY: [100, 0],
+			translateZ: 0,
+			opacity: [0, 1],
+			easing: "easeOutExpo",
+			duration: 1400,
+			delay: (el, i) => 300 + 30 * i
+		});
+	}
+});
 </script>
 
 <style lang="less" scoped>
+.hero-wrapper {
+	position: relative;
+	display: inline-block;
+	overflow: hidden;
+
+	.hero {
+		transform-origin: 0 100%;
+		display: inline-block;
+	}
+}
+
+p {
+	font-size: 18px;
+	letter-spacing: 4px;
+
+	ruby {
+		font-weight: 600;
+
+		cursor: default;
+
+		&:hover {
+			animation: wink 0.3s ease;
+		}
+	}
+}
+
 h1 {
-	font-size: 56px;
+	@media screen and (min-width: 1024px) {
+		font-size: 72px;
+	}
+
+	@media screen and (max-width: 1024px) {
+		font-size: 56px;
+	}
 	margin-top: 16px;
 	margin-bottom: 16px;
+}
 
-	& + p {
-    font-size: 18px;
-    letter-spacing: 4px;
+@keyframes wink {
+	from {
+		opacity: 1;
+	}
+	50% {
+		opacity: 0.2;
+	}
+	to {
+		opacity: 1;
+	}
+}
+
+@-webkit-keyframes wink {
+	from {
+		opacity: 1;
+	}
+	50% {
+		opacity: 0.2;
+	}
+	to {
+		opacity: 1;
 	}
 }
 </style>
