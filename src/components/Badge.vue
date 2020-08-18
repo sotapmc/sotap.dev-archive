99c92ba632c171175745241f18e431814c3d21ed
Subilan <suyuanliang1995@gmail.com>
2020-08-18 14:15:38 +0800

    Add component Badge

---
+++ src/components/Badge.vue
@@ -0,0 +1,25 @@
+<template>
+    <span class="badge" :style="'color:' + color + ';background-color:' + backgroundColor + ';'">
+        <slot/>
+    </span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    props: ["color", "backgroundColor"]
+});
+</script>
+
+<style lang="less" scoped>
+.badge {
+    display: inline-block;
+    padding-left: 4px;
+    padding-right: 4px;
+    border-radius: 4px;
+    margin-left: 8px;
+    margin-right: 8px;
+    font-size: 15px;
+    font-family: "Consolas", 'Courier New', Courier, monospace;
+}
+</style>
