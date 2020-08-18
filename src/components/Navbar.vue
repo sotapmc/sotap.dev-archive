da44b210484ca32c8ad4014ae8fbc1ecbd61280f
Subilan <suyuanliang1995@gmail.com>
2020-08-18 20:33:09 +0800

    Exchange pos

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,8 +8,8 @@
 			<a target="_blank" href="//sotap.org">官网</a>
 			<a target="_blank" href="//github.com/sotapmc">GitHub</a>
 			<a target="_blank" href="//docs.sotap.dev">文档</a>
-			<router-link to="/about">关于</router-link>
 			<router-link to="/resources">资源<Badge class="new-badge" color="white" backgroundColor="#e53935" fontSize="10px">new!</Badge></router-link>
+			<router-link to="/about">关于</router-link>
 		</div>
 	</div>
 </template>
