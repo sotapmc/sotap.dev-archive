2ed574da14027020747c7680cc56831ba784004f
Subilan <suyuanliang1995@gmail.com>
2020-08-19 11:52:55 +0800

    Remove target

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,9 +4,9 @@
         <p>以下列出联系我们的几种有效办法。</p>
         <p>注意：QQ 联系仅对 SoTap 玩家有效，其余情况请使用其它方式联系。</p>
         <h2>Sapherise</h2>
-        <p><ol><li>邮件 <a target="_blank" href="mailto:whj@skyers.cc">whj@skyers.cc</a></li><li>QQ</li></ol></p>
+        <p><ol><li>邮件 <a href="mailto:whj@skyers.cc">whj@skyers.cc</a></li><li>QQ</li></ol></p>
         <h2>Subilan</h2>
-        <p><ol><li>邮件 <a target="_blank" href="mailto:christophersubilan@gmail.com">christophersubilan@gmail.com</a></li><li>QQ</li></ol></p>
+        <p><ol><li>邮件 <a href="mailto:christophersubilan@gmail.com">christophersubilan@gmail.com</a></li><li>QQ</li></ol></p>
         <h2>Cropenfe</h2>
         <p>仅 QQ。</p>
         <h2>Constant137</h2>
